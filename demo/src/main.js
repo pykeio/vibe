0c2ca7fdfe75a9b96c2d54ea67d43bf69cdc36a2
Carson M <carson@pyke.io>
2022-11-09 12:07:57 -0600

    refactor: new demo!

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 const vibe = require('@pyke/vibe');
 
@@ -6,32 +6,37 @@ vibe.setup(app);
 
 app.whenReady().then(() => {
 	const mainWindow = new BrowserWindow({
-		width: 450,
-		height: 450,
+		width: 600,
+		height: 400,
 		center: true,
 		backgroundColor: '#00000000',
 		show: false,
-		autoHideMenuBar: true
+		autoHideMenuBar: true,
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: false
+		}
 	});
 
 	// Force dark theme for demonstration purposes.
 	vibe.forceTheme(mainWindow, 'dark');
 
-	vibe.applyEffect(mainWindow, 'acrylic');
-
-	mainWindow.setIcon(path.join(__dirname, '1f4ab.png'));
-
-	mainWindow.webContents.once('dom-ready', () => {
-		mainWindow.show();
-
-		setTimeout(() => {
-			vibe.applyEffect(mainWindow, 'mica');
-			setTimeout(() => {
-				vibe.clearEffects(mainWindow);
-				mainWindow.setBackgroundColor('#ffffff');
-			}, 5000);
-		}, 5000);
+	ipcMain.on('apply', (_, id) => {
+		mainWindow.setBackgroundColor('#00000000');
+		vibe.applyEffect(mainWindow, id);
+	});
+	ipcMain.on('clear', () => {
+		mainWindow.setBackgroundColor('#212121');
+		vibe.clearEffects(mainWindow);
 	});
 
+	mainWindow.webContents.once('dom-ready', () => {
+		// Set the background color to solid #212121 after creating the window, because apparently unified-acrylic and
+		// blurbehind need the window to be created with 0 alpha to work (or at least on Windows 11 22H2).
+		mainWindow.setBackgroundColor('#212121');
+		mainWindow.show();
+	});
+
+	mainWindow.setIcon(path.join(__dirname, 'icon.png'));
 	mainWindow.loadFile('src/index.html');
 });
